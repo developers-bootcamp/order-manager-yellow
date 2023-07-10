28b96b704cb4e36863d9f7def19132b3bd8e6e91
Esty <esty2048@gmail.com>
2023-07-10 16:22:57 +0300

    users and customers-apis

--- src/main/java/com/yellow/ordermanageryellow/Service/UsersService.java
+++ src/main/java/com/yellow/ordermanageryellow/Service/UsersService.java
@@ -1,40 +1,109 @@
 package com.yellow.ordermanageryellow.Service;
+
+import com.yellow.ordermanageryellow.DTO.MapStructMapper;
+import com.yellow.ordermanageryellow.DTO.UserDTO;
 import com.yellow.ordermanageryellow.Dao.UserRepository;
+import com.yellow.ordermanageryellow.exception.NotFoundException;
+import com.yellow.ordermanageryellow.exception.ObjectExistException;
+import com.yellow.ordermanageryellow.exception.WrongPasswordException;
 import com.yellow.ordermanageryellow.model.Users;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+
 
 @Service
 public class UsersService implements CommandLineRunner {
     private final UserRepository UserRepository;
+    private final MapStructMapper mapStructMapper;
+
     @Autowired
-    public UsersService(UserRepository UserRepository) {
+    public UsersService(UserRepository UserRepository, MapStructMapper mapStructMapper) {
         this.UserRepository = UserRepository;
+        this.mapStructMapper = mapStructMapper;
     }
+
     @Override
     public void run(String... args) {
         Users myModel = new Users("12");
         UserRepository.save(myModel);
     }
-    public  ResponseEntity<String> login(String email,String password){
-      try {
-          Users user = UserRepository.findUserByEmail(email);
-          if (user == null) {
-              return new ResponseEntity<>("user not found", HttpStatus.NOT_FOUND);
-          }
-              if (user.getPassword().equals(password))
-                  return new ResponseEntity<>(generateToken(user), HttpStatus.OK);
-              else
-                  return new ResponseEntity<>("wrong password", HttpStatus.UNAUTHORIZED);
-      }
-    catch (Exception e) {
-        return new ResponseEntity<>("unexpected error ", HttpStatus.INTERNAL_SERVER_ERROR);
+
+    @SneakyThrows
+    public String login(String email, String password) {
+        Users user = UserRepository.findUserByEmail(email);
+        if (user == null)
+            throw new NotFoundException("user not exist");
+        else if (!user.getPassword().equals(password))
+            throw new WrongPasswordException("invalid password");
+        else return generateToken(user);
+
     }
-}
-    public String generateToken(Users user){
-        return user.getCompanyId() + user.getId()+ user.getRoleId();
+
+
+    @SneakyThrows
+    public Users createNewUser(Users newUser) {
+        if (!findUser(newUser)) {
+            return UserRepository.save(newUser);
+        } else
+            throw new ObjectExistException("user is already exist");
     }
+
+    public String generateToken(Users user) {
+        return user.getCompanyId() + user.getId() + user.getRoleId();
+    }
+
+    public boolean findUser(Users user) {
+
+        Users foundUser = UserRepository.findUserByEmail(user.getAddress().getEmail());
+        if (foundUser == null)
+            return false;
+        return true;
+
+    }
+
+    @SneakyThrows
+    public void deleteUser(String id) {
+        if (UserRepository.existsById(id))
+            UserRepository.deleteById(id);
+        else
+            throw new NotFoundException("user not found");
+    }
+
+    @SneakyThrows
+    public Users updateUser(Users user) {
+        if (UserRepository.existsById(user.getId()))
+            return UserRepository.save(user);
+        else
+            throw new NotFoundException("user not found");
+    }
+
+    @SneakyThrows
+    public HashMap<String, String> getCustomerByNames(String prefix) {
+
+        //company and role will be taken from token
+        List<Users> users = UserRepository.findByFullNameContainingAndCompanyIdAndRoleId(prefix, "1", "1");
+        HashMap<String, String> userMap = new HashMap<>();
+        for (Users user : users) {
+            userMap.put(user.getId(), user.getFullName());
+        }
+        return userMap;
+    }
+
+    @SneakyThrows
+    public List<UserDTO> getCustomers(int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        //company and role will be taken from token
+        Page<Users> users = UserRepository.findAllByCompanyIdAndRoleId("1", "1", pageable);
+        return users.map(mapStructMapper::usersToUserDTO).getContent();
+    }
+
+
 }
--- src/main/java/com/yellow/ordermanageryellow/controller/UserController.java
+++ src/main/java/com/yellow/ordermanageryellow/controller/UserController.java
@@ -1,24 +1,102 @@
 package com.yellow.ordermanageryellow.controller;
 
+import com.yellow.ordermanageryellow.DTO.MapStructMapper;
+import com.yellow.ordermanageryellow.DTO.UserDTO;
 import com.yellow.ordermanageryellow.Service.UsersService;
+import com.yellow.ordermanageryellow.exception.NotFoundException;
+import com.yellow.ordermanageryellow.exception.ObjectExistException;
+import com.yellow.ordermanageryellow.model.Users;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/User")
 public class UserController {
 
     private final UsersService usersService;
+    private final MapStructMapper mapStructMapper;
+
     @Autowired
-    public UserController(UsersService usersService){
-        this.usersService=usersService;
-    }
-    @GetMapping("/login")
-    public ResponseEntity<String> login(@RequestParam  String password, @RequestParam String email){
-        return usersService.login(email,password);
+    public UserController(UsersService usersService, MapStructMapper mapStructMapper) {
+        this.usersService = usersService;
+        this.mapStructMapper = mapStructMapper;
     }
 
+    @GetMapping("/login")
+    public ResponseEntity<String> login(@RequestParam String password, @RequestParam String email, @RequestHeader String token) {
+        try {
+            return ResponseEntity.ok().body(usersService.login(email, password));
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createNewUser(@RequestBody Users newUser, @RequestHeader String token) {
+        try {
+
+            usersService.createNewUser(newUser);
+        } catch (ObjectExistException e) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+        return ResponseEntity.ok().body("success");
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id, @RequestHeader String token) {
+        try {
+            usersService.deleteUser(id);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(id);
+    }
+
+    @PutMapping()
+    public ResponseEntity updateCustomer(@RequestBody Users user, @RequestHeader String token) {
+        try {
+            usersService.updateUser(user);
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+        return ResponseEntity.status(HttpStatus.OK).body("update success " + user.getFullName());
+
+    }
+
+    @GetMapping("/{pageNumber}/{pageSize}")
+    public ResponseEntity<List<UserDTO>> getAllCustomers(@PathVariable int pageNumber, @PathVariable int pageSize, @RequestHeader String token) {
+        List<UserDTO> customers;
+        try {
+            customers = usersService.getCustomers(pageNumber, pageSize);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(customers);
+    }
+
+    @GetMapping("/customersNames")
+    public ResponseEntity<HashMap<String, String>> getCustomersByPrefix(@RequestParam String prefix, @RequestHeader String token) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(usersService.getCustomerByNames(prefix));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+
+    }
 
 
 }
