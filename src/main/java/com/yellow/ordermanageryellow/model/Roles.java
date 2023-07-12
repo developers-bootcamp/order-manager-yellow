391c731e2dfff28c781c644e00d4ff0d514d4881
AyalaSprei <ayala0bsd@gmail.com>
2023-07-12 14:53:16 +0300

    Jwt Token class is ready

--- src/main/java/com/yellow/ordermanageryellow/model/Roles.java
+++ src/main/java/com/yellow/ordermanageryellow/model/Roles.java
@@ -1,22 +1,18 @@
 package com.yellow.ordermanageryellow.model;
 
+import lombok.Data;
 import org.springframework.data.mongodb.core.annotation.Collation;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-enum name{ Admin,employee,customer};
+enum RoleNames{ADMIN, EMPLOYEE, CUSTOMER};
+@Data
 @Collation
 @Document(collection = "Roles")
 
 public class Roles {
     private String id;
-    private name name;
+    private RoleNames name;
     private String desc;
-    @DBRef
     private AuditData auditData;
-
-
-    public Roles(String id) {
-        this.id = id;
-    }
 }
