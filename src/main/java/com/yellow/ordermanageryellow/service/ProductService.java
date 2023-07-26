48986e61bbb00e75d3ff98f6aeaf2264be42baa7
MaayanShore <maayanshore@gmail.com>
2023-07-26 13:27:23 +0300

    top_sold_product_fix

--- src/main/java/com/yellow/ordermanageryellow/service/ProductService.java
+++ src/main/java/com/yellow/ordermanageryellow/service/ProductService.java
@@ -1,11 +1,11 @@
 package com.yellow.ordermanageryellow.service;
 
+
 import com.yellow.ordermanageryellow.dao.ProductRepository;
 import com.yellow.ordermanageryellow.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
-
 
 @Service
 public class ProductService {
