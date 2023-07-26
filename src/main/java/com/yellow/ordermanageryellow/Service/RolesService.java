6da1ad220a203db4f7a5e38c65b3382900b7a520
AyalaSprei <ayala0bsd@gmail.com>
2023-07-26 10:15:31 +0300

    t

--- src/main/java/com/yellow/ordermanageryellow/Service/RolesService.java
+++ src/main/java/com/yellow/ordermanageryellow/Service/RolesService.java
@@ -1,15 +1,12 @@
-
-package com.yellow.ordermanageryellow.Service;
-
-import com.yellow.ordermanageryellow.Dao.RolesRepository;
-import com.yellow.ordermanageryellow.model.Roles;
-import com.yellow.ordermanageryellow.model.Users;
+package com.yellow.ordermanageryellow.service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+import com.yellow.ordermanageryellow.dao.RolesRepository;
 
 @Service
 public class RolesService {
-    private RolesRepository RolesRepository;
+    @Autowired
+    private  RolesRepository RolesRepository;
 
-}
+private String a;}
+//nothing try commit
