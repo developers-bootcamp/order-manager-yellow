a2ba1cad07c207a2892db3c5bdb89cfcb0e960e1
RachelLivne <rachel01576@gmail.com>
2023-08-06 12:07:21 +0300

    getOrderById

--- src/main/java/com/yellow/ordermanageryellow/OrderManagerYellowApplication.java
+++ src/main/java/com/yellow/ordermanageryellow/OrderManagerYellowApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-		@EnableMongoRepositories
-		@SpringBootApplication
-		public class OrderManagerYellowApplication {
+@EnableMongoRepositories
+@SpringBootApplication
+public class OrderManagerYellowApplication {
 
-			public static void main(String[] args) {
-				SpringApplication.run(OrderManagerYellowApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerYellowApplication.class, args);
 	}
 
 }
-
