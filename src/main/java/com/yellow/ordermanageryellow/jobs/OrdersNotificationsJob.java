e588b8f9b66ff055f897f22754aaeba7e62cbc5f
AyalaSprei <ayala0bsd@gmail.com>
2023-09-05 11:51:12 +0300

    finished

--- src/main/java/com/yellow/ordermanageryellow/jobs/OrdersNotificationsJob.java
+++ src/main/java/com/yellow/ordermanageryellow/jobs/OrdersNotificationsJob.java
@@ -4,25 +4,30 @@ import com.yellow.ordermanageryellow.Dao.OrdersRepository;
 import com.yellow.ordermanageryellow.model.Orders;
 import com.yellow.ordermanageryellow.service.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 import com.yellow.ordermanageryellow.Exception.*;
 import java.util.List;
+import java.util.Optional;
 
 public class OrdersNotificationsJob {
     @Autowired
     private OrdersService orderService;
     @Autowired
     private OrdersRepository ordersRepository;
+    @DBRef  List<Orders> ordersToNotify;
     @Scheduled(cron = "*/2 * * * * *")
     @Transactional
     public void OrderNotifications() throws NotFoundException {
         List<Orders> ordersToNotify = ordersRepository.findByNotificationFlag(true);
+
         for (Orders order : ordersToNotify) {
             Notification(order);
             order.setNotificationFlag(false);
-            ordersRepository.save(order);
         }
+      ordersRepository.saveAll(ordersToNotify);
     }
     private void Notification(Orders order) {
         mockNotification(order);
